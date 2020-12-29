fff110e0a38ed945bbb6dc19fcc7718b8193c115
hlin91 <hlin91@outlook.com>
2020-12-29 10:06:19 -0800

    Updating file paths

--- environment.py
+++ environment.py
@@ -1,6 +1,6 @@
-WED_FROG = "wedFrog.jpg" # Path to wednesday frog image
-PROFILE_IMG = "mao.png" # Path to profile picture
+WED_FROG = "./wedFrog.jpg" # Path to wednesday frog image
+PROFILE_IMG = "./mao.png" # Path to profile picture
 # Paths to three different versions of Last Christmas
-CHRISTMAS_1 = "christmas.mp3" 
-CHRISTMAS_2 = "christmas2.mp3"
-CHRISTMAS_3 = "christmas3.mp3"
+CHRISTMAS_1 = "./christmas.mp3" 
+CHRISTMAS_2 = "./christmas2.mp3"
+CHRISTMAS_3 = "./christmas3.mp3"
