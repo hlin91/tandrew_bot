ee919f51e6f246581d16d493fcbe4d542e5ccfaf
hlin91 <hlin91@outlook.com>
2020-12-31 23:05:53 -0800

    Changed default profile pic

--- environment.py
+++ environment.py
@@ -1,5 +1,5 @@
 WED_FROG = "./wedFrog.jpg" # Path to wednesday frog image
-PROFILE_IMG = "./mao.png" # Path to profile picture
+PROFILE_IMG = "./tuna.png" # Path to profile picture
 # Paths to three different versions of Last Christmas
 CHRISTMAS_1 = "./christmas.mp3" 
 CHRISTMAS_2 = "./christmas2.mp3"
